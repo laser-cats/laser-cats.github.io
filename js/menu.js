f6275d061b471119d323122011c2a2fa1470008d
Nicholas Kapur <npkapur@Nicholass-MBP-5.attlocal.net>
2016-09-11 15:06:34 -0400

    full screen test

--- js/menu.js
+++ js/menu.js
@@ -40,6 +40,10 @@ var Menu = {
     }
     else
     {
+    	this.game.scale.setMaximum();
+        this.game.scale.setScreenSize(true);
+        this.game.scale.pageAlignVertically = false;
+        this.game.scale.pageAlignHorizontally = false;
         game.scale.startFullScreen(false);
     }
     },
